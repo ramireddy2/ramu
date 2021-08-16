27264f6e13670c2f17aedc4a2bef49b8318e2087
bhadram.komali <ramireddymandava@gmail.com>
2021-08-16 12:34:19 +0530

    added js files

---
+++ js files/1-3.js
@@ -0,0 +1,5 @@
+var x=10;
+var y=15;
+alert(x);
+console.log(y);
+document.getElementById("file").innerHTML="fsjoigjek gharlihgapjgr";
---
+++ js files/1-4.js
@@ -0,0 +1,3 @@
+function myfunction(){
+    document.getElementById("js").innerHTML="replace the file";
+}
